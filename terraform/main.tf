3e86890901e62bb4726543c4db8db74a53122035
mehemken <mehemken@gmail.com>
2022-03-23 09:32:51 -0400

    new sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,15 @@
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Main"
+  }
 }
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/25"
+  cidr_block = "10.0.0.0/25"
 
   tags = {
     Name = "Main"
@@ -18,8 +22,16 @@ resource "aws_subnet" "main" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  subnet_id     = aws_subnet.main.id
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+# variables
+
+variable "my_ip" {
+  type    = string
+  default = ""
 }
---
+++ terraform/sg.tf
@@ -0,0 +1,13 @@
+resource "aws_security_group" "allow_wireguard" {
+  name        = "allow_wireguard"
+  description = "Allow my own wireguard traffic."
+  vpc_id      = aws_vpc.main.id
+}
+
+resource "aws_security_group_rule" "wg_in" {
+  type        = "ingress"
+  from_port   = 0
+  to_port     = 65535
+  protocol    = "tcp"
+  cidr_blocks = ["${var.my_ip}/32"]
+}
